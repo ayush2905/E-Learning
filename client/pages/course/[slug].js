f6f917d23aef47a5a4bb097172661d109c1cd79f
ayush2905 <ayushagrawal2905@gmail.com>
2023-06-20 20:18:38 +0530

    Single course view

--- client/pages/course/[slug].js
+++ client/pages/course/[slug].js
@@ -1,18 +1,68 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import { Badge } from "antd";
+import ReactPlayer from "react-player";
 
 const SingleCourse = ({ course }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [preview, setPreview] = useState("");
   const router = useRouter();
   const { slug } = router.query;
+  const {
+    name,
+    description,
+    instructor,
+    updatedAt,
+    lessons,
+    image,
+    price,
+    paid,
+    category,
+  } = course;
 
   return (
     <>
-      <diV className="container-fluid">
+      {/*<pre>{JSON.stringify(course, null, 4)}</pre>*/}
+      <div className="jumbotron bg-primary square">
         <div className="row">
-          <pre>{JSON.stringify(course, null, 4)}</pre>
+          <div className="col-md-8">
+            <h1 className="text-light font-weight-bold">{name}</h1>
+            <p className="lead">
+              {description && description.substring(0, 160)}...
+            </p>
+            <Badge
+              count={category}
+              style={{ backgroundColor: "#03a9f4" }}
+              className="pb-4 mr-2"
+            />
+            <p>Create by {instructor.name}</p>
+            <p> Last updated: {new Date(updatedAt).toLocaleDateString()}</p>
+            <h4 className="text-light">{paid ? "$" + price : "Free"}</h4>
+          </div>
+          <div className="col-md-4">
+            {lessons[0].video && lessons[0].video.Location ? (
+              <div>
+                <ReactPlayer
+                  className="react-player-div"
+                  url={lessons[0].video.Location}
+                  light={image.Location}
+                  width="100%"
+                  height="225px"
+                />
+              </div>
+            ) : (
+              <>
+                <img
+                  src={image.Location}
+                  alt={name}
+                  className="img img-fluid"
+                />
+              </>
+            )}
+          </div>
         </div>
-      </diV>
+      </div>
     </>
   );
 };
